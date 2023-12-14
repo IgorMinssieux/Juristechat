05772ac48de39f6904f100904093da6e94121878
IgorMinssieux <74765620+IgorMinssieux@users.noreply.github.com>
2023-12-14 21:30:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,7 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
-        client = OpenAI(api_key="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")
+        client = OpenAI(api_key)
         for response in client.completions.create(
             model="gpt-3.5-turbo"):
             full_response += response.choices[0].delta.get("content", "")
