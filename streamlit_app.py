c35b509db4e5200cfef62371fa91799a19e8bf5f
IgorMinssieux <74765620+IgorMinssieux@users.noreply.github.com>
2023-12-14 21:15:54 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,14 @@
-import os
+import openai
 import streamlit as st
-from openai import OpenAI
-# Set your OpenAI API key
 
 with st.sidebar:
     st.title('🤖💬 OpenAI Chatbot')
     if 'OPENAI_API_KEY' in st.secrets:
         st.success('API key already provided!', icon='✅')
-        client = OpenAI(
-            api_key=os.environ['OPENAI_API_KEY'],
-        )
-        
+        openai.api_key = st.secrets['OPENAI_API_KEY']
     else:
         openai.api_key = st.text_input('Enter OpenAI API token:', type='password')
-        if not (openai.api_key.startswith('sk-') and len(openai.api_key) == 51):
+        if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
             st.warning('Please enter your credentials!', icon='⚠️')
         else:
             st.success('Proceed to entering your prompt message!', icon='👉')
@@ -32,12 +27,11 @@ if prompt := st.chat_input("What is up?"):
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
-
-        try:
-            response = client.completions.create(model='curie')
-            full_response += response.choices[0].text.strip()
-            message_placeholder.markdown(full_response)
-        except Exception as e:
-            st.error(f"An error occurred: {str(e)}")
-
+        for response in openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[{"role": m["role"], "content": m["content"]}
+                      for m in st.session_state.messages], stream=True):
+            full_response += response.choices[0].delta.get("content", "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
     st.session_state.messages.append({"role": "assistant", "content": full_response})
